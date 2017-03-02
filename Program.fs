9ce3059fed45503448e4718eb160fab0d2519132
Enzojz <enzojz@gmail.com>
2017-03-02 06:57:25 +0100

    Format

--- Program.fs
+++ Program.fs
@@ -2,17 +2,33 @@
 open System
 
 type Lua = 
-    | Val of int
-    | Str of string
-    | Ent of (string * Lua)
-    | Arr of Lua list
+    | V of int
+    | S of string
+    | P of (string * Lua)
+    | A of Lua list
+    | F of (string * Lua)
+    | C of (bool * Lua)
 
-let rec printLua indent (node: Lua) =
+let rec printLua indent (node : Lua) = 
+    let ind str = 
+        function 
+        | true -> (Environment.NewLine + (String.replicate indent "  ") + str)
+        | false -> str
     match node with
-    | Val num -> sprintf "%d" num
-    | Str str -> str
-    | Ent (name, x) -> name + " = " + (printLua indent x)
-    | Arr ls -> "{" + (ls |> List.map (printLua indent) |> String.concat ", ") + "}" + Environment.NewLine
+    | V num -> sprintf "%d" num
+    | S str -> @"""" + str + @""""
+    | P(name, x) -> name + " = " + (printLua (indent + 1) x)
+    | A ls -> 
+        "{ " + (ls
+                |> List.map (printLua (indent + 1))
+                |> String.concat ", ")
+        |> fun str -> str + (ind " }" (str.Contains(Environment.NewLine)))
+    | F(f, x) -> (sprintf "function %s() return " f) + (printLua (indent + 1) x) + " end"
+    | C(c, x) -> 
+        if c then (printLua (indent + 1) x)
+        else ""
+    |> fun rs -> ind rs (rs.Length > 30)
+
 
 type ByteData = 
     { normals : byte list;
@@ -49,14 +65,9 @@ let toBytes f vectors =
     |> List.map (f >> toByte)
     |> flatten
 
-let revT = 
-    Matrix4x4(
-        1.0f, 0.0f, 0.0f, 0.0f, 
-        0.0f, 0.0f, -1.0f, 0.0f, 
-        0.0f, 1.0f, 0.0f, 0.0f, 
-        0.0f, 0.0f, 0.0f, 1.0f)
+let revT = Matrix4x4(1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f)
 
-let convertMesh ((mesh, transform) : (Mesh * Matrix4x4)) = 
+let convertMesh ((mesh, transform) : Mesh * Matrix4x4) = 
     let normals = mesh.Normals |> Seq.toList
     let vertices = mesh.Vertices |> Seq.toList
     let tangents = mesh.Tangents |> Seq.toList
@@ -87,46 +98,45 @@ let meshGen (meshes : (Material * ByteData) list) =
           tangents = counts.vertices + counts.uv0 + counts.normals;
           uv1 = counts.vertices + counts.uv0 + counts.normals + counts.tangents }
     
+    let indiceAttr count offset = 
+        A [ P("count", V count);
+            P("offset", V offset) ]
+    
     let rec subMeshIndices (meshes : (Material * ByteData) list) offset result = 
         match meshes with
-        | [] -> result
+        | [] -> A result
         | (m, r) :: rest -> 
             let indiceLength = r.nbFace * 3 * sizeof<int>
-            result @ [ @"       {";
-                       @"           indices = {";
-                       @"               normal =   { count = " + d indiceLength + ", offset = " + d offset + ", },";
-                       @"               position = { count = " + d indiceLength + ", offset = " + d offset + ", },";
-                       @"               tangent =  { count = " + d indiceLength + ", offset = " + d offset + ", },";
-                       @"               uv0 =      { count = " + d indiceLength + ", offset = " + d offset + ", }," ]
-                     @ if r.uv1.Length = 0 then []
-                       else [ @"               uv1 =      { count = " + d indiceLength + ", offset = " + d offset + ", }," ]
-                       @ [ @"           },";
-                           @"           materials = { """ + m.Name + @".mtl"" },";
-                           @"       }," ]
+            result @ [ A [ P("indices", 
+                             A [ P("normal", indiceAttr indiceLength offset);
+                                 P("position", indiceAttr indiceLength offset);
+                                 P("tangent", indiceAttr indiceLength offset);
+                                 P("uv0", indiceAttr indiceLength offset);
+                                 C(r.uv1.Length > 0, P("uv1", indiceAttr indiceLength offset)) ]);
+                           P("materials", A [ S(m.Name + @".mtl") ]) ] ]
             |> subMeshIndices rest (offset + indiceLength)
     
-    ([ @"function data() return {";
-       @"   animations = {";
-       @"   },";
-       @"   matConfigs = { { 0, }, },";
-       @"   vertexAttr =";
-       @"   {";
-       @"      normal =       { count = " + d counts.normals + ", numComp = 3, offset = " + d offsets.normals + ", },";
-       @"      position =     { count = " + d counts.vertices + ", numComp = 3, offset = " + d offsets.vertices + ", },";
-       @"      tangent =      { count = " + d counts.tangents + ", numComp = 4, offset = " + d offsets.tangents + ", },";
-       @"      uv0 =          { count = " + d counts.uv0 + ", numComp = 2, offset = " + d offsets.uv0 + ", },";
-       @"      uv1 =          { count = " + d counts.uv1 + ", numComp = 2, offset = " + d offsets.uv1 + ", },";
-       @"   },";
-       @"   subMeshes = { " ]
-     @ (subMeshIndices meshes (offsets.uv1 + counts.uv1) []) @ [ @"   },"; @"} end" ])
-    |> String.concat Environment.NewLine
+    let vertexAttr count numComp offset = A(P("count", V count) :: P("numComp", V numComp) :: [ P("offset", V offset) ])
+    F("data", 
+      A [ P("animations", A []);
+          P("matConfigs", A [ A (List.init meshes.Length (fun _ -> V 0)) ]);
+          P("subMeshes", subMeshIndices meshes (offsets.uv1 + counts.uv1) []);
+          P("vertexAttr", 
+            A [ P("normal", vertexAttr counts.normals 3 offsets.normals);
+                P("position", vertexAttr counts.vertices 3 offsets.vertices);
+                P("tangent", vertexAttr counts.tangents 4 offsets.tangents);
+                P("uv0", vertexAttr counts.uv0 2 offsets.uv0);
+                C (counts.uv1 > 0, P("uv1", vertexAttr counts.uv1 2 offsets.uv1)) ]) ])
+    |> printLua 0
+    |> fun str -> str.Trim()
 
 let blobGen mesh = 
     let indices = 
         [ 0..(mesh.nbFace * 3 - 1) ]
         |> List.map (BitConverter.GetBytes >> Array.toList)
         |> flatten
-    mesh.vertices @ mesh.uv0 @ mesh.normals @ mesh.tangents @ mesh.uv1 @ indices@ indices@ indices@ indices @ indices |> List.toArray
+    mesh.vertices @ mesh.uv0 @ mesh.normals @ mesh.tangents @ mesh.uv1 @ indices  |> List.toArray
+
 
 let prepareMesh (meshes : (Material * ByteData list) list) = 
     let mergeMeshes (meshes : ByteData list) = 
@@ -201,6 +211,7 @@ let convert (inputFile : string) =
     assimpImporter.Dispose()
     printfn "======== Finished ========"
 
+
 [<EntryPoint>]
 let main argv = 
     let mutable start = true
@@ -226,3 +237,4 @@ let main argv =
         printfn @"Press ""R"" to repeat, any other key to close..."
         start <- Console.ReadKey().Key = ConsoleKey.R
     0 // return an integer exit code
+
